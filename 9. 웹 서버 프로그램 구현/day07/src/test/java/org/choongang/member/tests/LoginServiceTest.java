780f24935057f5fca19a727ce29434193f1520ed
sswum <l5450won@naver.com>
2024-06-20 09:53:02 +0900

    로그인 기능 테스트를 위한 테스트 클래스 생성

---
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/tests/LoginServiceTest.java
@@ -0,0 +1,18 @@
+package org.choongang.member.tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+//로그인을 문제없이 하기 위해서 만듦
+@ExtendWith(MockitoExtension.class)
+@DisplayName("로그인 기능 테스트")
+public class LoginServiceTest {
+    @DisplayName("로그인 성공 시 예외가 발생하지 않음")
+    @Test
+    void successTest() {
+
+
+    }
+}
