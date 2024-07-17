f2f255ce1bcf0584babaa29623cb3d980078b996
sswum <l5450won@naver.com>
2024-07-17 16:56:24 +0900

    [스프링] mockmvc TEST

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/api/controllers/ApiMemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/api/controllers/ApiMemberController.java
@@ -6,7 +6,10 @@ import org.choongang.member.controllers.RequestJoin;
 import org.choongang.member.entities.Member;
 import org.choongang.member.mappers.MemberMapper;
 import org.choongang.member.services.JoinService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -22,10 +25,16 @@ public class ApiMemberController {
     private final MemberMapper mapper;
     private final JoinService joinService;
 
+
     @PostMapping // POST /api/member
-    public void join(RequestJoin form) {
-        log.info(form.toString());
+    public ResponseEntity join(@RequestBody RequestJoin form) {
+
+        joinService.process(form);
+
+        // 응답 코드 201, 출력 바디 X
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
 
     @GetMapping("/info/{email}")
     public Member info(@PathVariable("email") String email) {
@@ -35,17 +44,23 @@ public class ApiMemberController {
     }
 
     @GetMapping("/list") //여러 사용자 만들기
-    public List<Member> list() {
+    public ResponseEntity<List<Member>> list() {
         List<Member> members = IntStream.rangeClosed(1, 10)
                 .mapToObj(i -> Member.builder()
                         .email("user" + i + "@test.org")
-                        //제일 민감한 건 비밀번호 =>민감한 부분은 json을 방지하는 애노테이션이 있음
                         .password("12345678")
                         .userName("사용자" + i)
                         .regDt(LocalDateTime.now())
                         .build())
-                .toList();
-        return members;
+                .toList(); // 상세하게 상태코드와 데이터를 출력하게끔 넣었땅.
+
+        HttpHeaders headers =new HttpHeaders(); //헤더
+        headers.add("t1", "v1");
+        headers.add("t2", "v2");
+
+
+
+        return new ResponseEntity<>(members, headers, HttpStatus.OK);
     }
 
 
